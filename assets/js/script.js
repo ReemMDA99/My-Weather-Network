adf39083f4a435bbfc329ce02ababa11322e0298
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-01 10:48:29 -0600

    fetch apikey

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//create api key and function variable
+var apiKey ="4515771b12df194d0484c403031018ac";
+var locationInput= document.querySelector("#locationInput");
+var locationName= document.querySelector("#locationName");
+var locationList=[];
+
