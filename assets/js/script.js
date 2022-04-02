562343431f5a31686183937b0ac83ffabe02a256
Reem Ahmed <reem.mda0909@gmail.com>
2022-04-02 08:59:30 -0600

    create present weather content:temp, humidity, windspeed

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,16 +6,63 @@ var locationName = document.querySelector("#locationName");
 // store city location
 var locationList =[];
 
-// get geological coordinate
-var showWeather= function(locationName) {
-// store api url to get geological coordinate of the city
-var apiLocationUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=" + apiKey;
-// get location data from api url
-fetch(apiLocationUrl)
-.then (function(response) {
-    if(response.ok) {
-        response.json()
-        .then(function(data) {
-            if (data[0] === null) {
-                window.alert("The city name is invalid. Please enter a valid city name.");
-                return;
+// get apiLocationURL coordinate
+var showWeather= async function() {
+    var apiLocationURL ="https://api.openweathermap.org/data/2.5/weather?q=" + locationName + "&units=metric&appid=" + apiKey;
+    var result = await $.ajax({
+        url: apiLocationURL,
+        method: "GET"
+    })
+    // get location data from api url
+    fetch(apiLocationURL)
+    .then(function(result) {
+    // request was successful
+    if (result.ok) {
+      result.json().then(function() {
+        showWeather();
+      })
+    } else {
+    window.alert("Sorry,no data found for " + locationName + ". Try other city.");
+}
+console.log(result);
+});
+//create var for latitude and longitude
+var long = result.coord.lon;
+var lati = result.coord.lat;
+
+//create a var for current date
+var d = new Date(); 
+var today = (d.getMonth()+1) + "/" + d.getDate() + "/" + d.getFullYear();
+
+//using jQuery create var elements to run open weather api
+var todaysForecast = $("<div class='card-body' id='currentWeather'>");
+var getPresentLocation = result.name;
+
+//Create present weather icon and display icon function
+var getPresentWeatherIcon = result.weather[0].icon;
+var showPresentWeatherIcon = $("<img src = http://openweathermap.org/img/wn/" + getPresentWeatherIcon + "@2x.png />");
+
+//Create var for present location
+var presentLocationEl = $("<h3 class='card-body'>")
+.text(getPresentLocation+"("+today+")");
+presentLocationEl.append(showPresentWeatherIcon);
+todaysForecast.append(presentLocationEl);
+
+//Create var for present temperature
+var getTemp = result.main.temp;
+var tempEl = $("<p class='card-text'>")
+.text("Temperature: " +getTemp+ "° C");
+todaysForecast.append(tempEl);
+
+//Create var for current wind speed
+var getWindSpeed = result.wind.speed;
+var windEl = $("<p class='card-text'>")
+.text("Wind: "+getWindSpeed+" m/s");
+todaysForecast.append(windEl);
+
+//Create var for present humidity
+var getHumidity = result.main.humidity;
+var humidEl = $("<p class='card-text'>")
+.text("Humidity: " + getHumidity + "%");
+todaysForecast.append(humidEl);
+}
